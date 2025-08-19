35cdcf7d37b30cdb1bf5347857ae2ae9f34c58d9
passing7by <jinam0717@gmail.com>
2025-08-19 14:30:49 +0900

    Add: 프로젝트 기본 설정 추가

---
+++ src/main/java/com/shelter/app/PetShelterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shelter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShelterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShelterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shelter/app/PetShelterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelter.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
