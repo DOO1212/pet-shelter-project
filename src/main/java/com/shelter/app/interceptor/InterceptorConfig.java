29a8c45d2f2987b60eb8fa269c6e7c2231638828
passing7by <jinam0717@gmail.com>
2025-08-22 13:00:19 +0900

    Add: 동물 등록, 수정 페이지 요청 시 관리자 권한인지 체크하는 기능 추가

--- src/main/java/com/shelter/app/interceptor/InterceptorConfig.java
+++ src/main/java/com/shelter/app/interceptor/InterceptorConfig.java
@@ -1,20 +1,21 @@
-package com.shelter.app.config;
+package com.shelter.app.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.shelter.app.interceptor.LogingInterceptor;
-
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 	@Autowired
 	private LogingInterceptor logingInterceptor;
+	@Autowired
+	private AdminAccessInterceptor adminAccessInterceptor;
 	
 	// 인터셉터들을 registry에 추가하는 메서드
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(logingInterceptor).addPathPatterns("/", "/member/**");
+		registry.addInterceptor(logingInterceptor).addPathPatterns("/", "/member/**", "/pet/**", "/donation/**", "/ask/**");
+		registry.addInterceptor(adminAccessInterceptor).addPathPatterns("/pet/add", "/pet/update", "/pet/delete");
 	}
 }
